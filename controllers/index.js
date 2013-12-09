fdecc9c60760f87b0241f3294bf8db4edd606a68
Avi Romanoff <aviromanoff@gmail.com>
2013-12-09 18:26:03 -0500

    Models and some groundwork

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,17 @@
 'use strict';
 
+var poll = require("../models/poll")
 
 module.exports = function (server) {
 
     server.get('/', function (req, res) {
         var model = { name: 'FastStraw' };
-        
-        res.render('index', model);
-        
+		res.render('index', model);  
+    });
+
+    server.get('/vote/:token', function (req, res) {
+        var model = { name: 'FastStraw' };
+		res.render('vote', model);
     });
 
 };
